1a7ca4394347ab0d598f8229623a2471df88d901
Tsigbey Samuel <tsigbeysamuel2@gmail.com>
2026-07-23 12:06:38 +0000

    Working with POST and GET

--- site.php
+++ site.php
@@ -6,21 +6,15 @@
     <title>Document</title>
 </head>
 <body>  
-  <form action="site.php" method="get">
-    Color: <input type="text" name="color"><br>
-    Plural Noun: <input type="text" name="pluralNoun"><br>
-    Celebrity: <input type="text" name="celebrity"><br>
+  <form action="site.php" method="post">
+    Password: <input type="password" name="password"><br>
+   
 <input type="submit">
   </form>
 <?php 
-$color = $_GET["color"];
-$pluralNoun = $_GET["pluralNoun"];
-$celebrity = $_GET["celebrity"];
-
-
-echo "Roses are $color <br>";
-echo "$pluralNoun are blue<br>";
-echo "I love $celebrity <br>";
+echo $_POST["password"];//POST is more secure than GET. 
+//Stops your info from showing up in the URL. 
+// Got passed between the client and the server
 ?>
 </body>
 </html>
