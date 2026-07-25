6438d3f82d8cdb2d6fc0ebd67313f2c0f536a90e
Tsigbey Samuel <tsigbeysamuel2@gmail.com>
2026-07-25 18:45:21 +0000

    Building a better calculator

--- site.php
+++ site.php
@@ -6,12 +6,33 @@
     <title>Document</title>
 </head>
 <body>  
-  
+  <form action="site.php" method="post">
+   First Number: <input type="number" name="num1">
+   OP: <input type="text" name="op">
+   Second Number: <input type="number" name="num2">
+
+   <input type="submit">
+</form> 
 <?php 
-function sayHi($num) {
-    return 1 + $num;
+$num1 = $_POST["num1"];
+$op = $_POST["op"];
+$num2 = $_POST["num2"];
+
+if($op == "+"){
+    echo $num + $num2;
 }
- echo sayHi(5);
+    elseif($op == "-"){
+        echo $num - $num2;
+    }
+     elseif($op == "/"){
+        echo $num / $num2;
+    }
+     elseif($op == "*"){
+        echo $num * $num2;
+    }
+    else{
+        echo "invalid operator";
+    }
 ?>
 </body>
 </html>
