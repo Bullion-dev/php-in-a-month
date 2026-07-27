cb37bb540551ffe04529d0044a3f503240a966df
Tsigbey Samuel <tsigbeysamuel2@gmail.com>
2026-07-27 11:18:12 +0000

    Working with class and objects

--- article-header.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* these templates will get populated
-with the data set in site.php */
-?>
-
-<h2><?php echo $title ?></h2>
-<h4><?php echo $author ?></h4>
-Word count :<?php echo $wordCount ?>
--- site.php
+++ site.php
@@ -7,9 +7,32 @@
 </head>
 <body>  
 <?php 
-$title = "My First Viral Post";
-$author = "Sam";
-$wordCount = 400; 
-include "article-header.php" ?>
+/* class is basically just a blueprint
+a spec of what a book is.
+we're basically defining a new data type */
+class Book {
+var $title;
+var $author;
+var $pages;
+}
+
+/* for here we're creating objects.
+it's an instance of a class. 
+The arrow operator -> (dash + greater-than,
+ no space between them) is what PHP uses to access 
+ an object's properties or methods*/
+
+$book1 = new Book;
+$book1->title = "How to stop worrying and start living";
+$book1->author = "Dale Carnegie";
+$book1->pages = 400;
+
+$book2 = new Book;
+$book2->title = "How to win friends and influence people";
+$book2->author = "Dale Carnegie";
+$book2->pages = 700;
+
+echo $book2->pages;
+ ?>
 </body>
 </html>
